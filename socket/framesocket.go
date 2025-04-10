740b16f3f97260daaf9547c63a5ea8840af14700
bn-k <karim@benhammou.me>
2025-04-09 22:52:47 -0500

    fix: change error logging from error to warning for websocket reads

--- socket/framesocket.go
+++ socket/framesocket.go
@@ -223,7 +223,7 @@ func (fs *FrameSocket) readPump(conn *websocket.Conn, ctx context.Context) {
 		if err != nil {
 			// Ignore the error if the context has been closed
 			if !errors.Is(ctx.Err(), context.Canceled) {
-				fs.log.Errorf("Error reading from websocket: %v", err)
+				fs.log.Warnf("Error reading from websocket: %v", err)
 			}
 			return
 		} else if msgType != websocket.BinaryMessage {
